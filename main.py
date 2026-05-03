4eb74cd658d79a27bf52c3817f9ea69fe5202b7c
KelsonCarvalho <139078935+Kelson-Carvalho@users.noreply.github.com>
2026-05-03 13:04:58 -0300

    Clarify SSL configuration comment for Android
    Updated SSL configuration comment for clarity.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.utils import platform
 
-# Configuração SSL para Android
+# Configuração SSL para evitar erros de conexão no Android
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 KV = '''
